0414c62b1565e2c01eb86bbe437ea0c3ff1f4ec6
Gabe <ghuinker@gmail.com>
2019-12-08 17:10:02 -0600

    Final commit!

--- tree.cpp
+++ tree.cpp
@@ -15,27 +15,15 @@ int main(int argc, char *argv[]){
   manager m;
   char *load_file;
 
-
   auto start = chrono::high_resolution_clock::now();
-
-
   load_file = NULL;
 
-  //Debug
   if (argc < 2) {
-    char tempStr[] = "test.txt";
-    load_file = tempStr;
+    std::cout << "Please enter file name for tree initialization and actions.\n";
+    exit(-1);
   } else {
     load_file = argv[1];
   }
-
-  // Prod
-  // if (argc < 2) {
-  //   std::cout << "Please enter file name for tree initialization and actions.\n";
-  //   exit(-1);
-  // } else {
-  //   load_file = argv[1];
-  // }
 
   read_instruction(&i, load_file);
   init_tree(&t, &i);
